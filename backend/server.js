ef2d0b5a2bcb13774916e814e919fd2b1592f839
DeepanshiSaini110 <deepanshi4uever27@gmail.com>
2026-05-21 03:55:15 +0000

    final vercel fix

--- backend/server.js
+++ backend/server.js
@@ -36,39 +36,12 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function(origin, callback) {
-
-      if (
-        !origin ||
-        allowedOrigins.includes(origin)
-      ) {
-        callback(null, true);
-      } else {
-        callback(
-          new Error("CORS blocked")
-        );
-      }
-
-    },
-    credentials: true,
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-      "OPTIONS"
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization"
-    ]
+    origin: allowedOrigins,
+    credentials: true
   })
 );
 
-app.options("*", cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+
 
 app.use(express.json());
 // io.on("connection", (socket) => {
