f737ea93d40c9c881f49f4f29be0181e4c608c5a
DeepanshiSaini110 <deepanshi4uever27@gmail.com>
2026-05-21 05:50:01 +0000

    final vercel fix

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 const allowedOrigins = [
   "https://mass-messenger-up3h.vercel.app",
-  "https://mass-messenger-up3h-g96gz5a77-deepanshisaini110s-projects.vercel.app"
+  "https://mass-messenger-l4m3-eqgli97zs-deepanshisaini110s-projects.vercel.app"
 ];
 
 // const io = new Server(server, {
