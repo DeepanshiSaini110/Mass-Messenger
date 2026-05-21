6e9ec3202f345d4ce4a0c2e7fc566d435520b5e0
DeepanshiSaini110 <deepanshi4uever27@gmail.com>
2026-05-21 05:59:58 +0000

    final vercel fix

--- frontend/src/pages/api.js
+++ frontend/src/pages/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://mass-messenger-l4m3-eqgli97zs-deepanshisaini110s-projects.vercel.app",
+    "https://mass-messenger-j8d1vc268-deepanshisaini110s-projects.vercel.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
